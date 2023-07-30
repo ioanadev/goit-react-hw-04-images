90a7b87e1886356e596209db1f9a0913e48fd3f7
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-07-31 00:05:55 +0300

    corection

--- src/components/ButtonLoadMore/ButtonLoadMore.js
+++ src/components/ButtonLoadMore/ButtonLoadMore.js
@@ -1,16 +1,13 @@
 import './ButtonLoandMore.css';
-import { Component } from 'react';
-// Componenta Button
-export class Button extends Component {
-  render() {
-    const { onClick } = this.props;
 
-    return (
-      <div className="button-container">
-        <button type="button" className="button" onClick={onClick}>
-          Load more
-        </button>
-      </div>
-    );
-  }
-}
+// Componenta Button
+
+export const Button = ({ onClick }) => {
+  return (
+    <div className="button-container">
+      <button type="button" className="button" onClick={onClick}>
+        Load more
+      </button>
+    </div>
+  );
+};
